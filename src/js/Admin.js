94d8b831d926a06fb95a8bdc6a671a354d5a3077
Vinton Teo <taulago@ymail.com>
2019-05-30 22:41:37 -0600

    Change Admin class to accept hotel as a parameter.

--- src/js/Admin.js
+++ src/js/Admin.js
@@ -1,9 +1,9 @@
-import Hotel from '.Hotel';
-
-class Admin extends Hotel {
-  constructor(bookings, rmServices, customers) {
-    super(bookings, rmServices, customers)
+class Admin {
+  constructor(customers, hotel) {
     this.customers = customers;
+    this.rooms = hotel.bookings.rooms;
+    this.bookings = hotel.bookings;
+    this.services = hotel.services;
     this.currentCustomer;
   }
 }
