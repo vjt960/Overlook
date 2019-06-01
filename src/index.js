e5bda9c740e2c5633e64af5c49478e67cb382a56
Vinton Teo <taulago@ymail.com>
2019-06-01 15:52:17 -0600

    Clean index.js of unneeded comments. Structure html.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
 
 import {xData as data} from '.././fetch-data/data';
@@ -11,17 +7,10 @@ import RoomServices from './js/RoomServices';
 import Hotel from './js/Hotel';
 import CustomerRepo from './js/CustomerRepo';
 import Admin from './js/Admin';
-
-
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-// import CustomerRepo from './js/CustomerRepo';
-
-console.log('This is the JavaScript entry file - your code begins here.');
-
 
 $(document).ready(() => {
   let delay;
@@ -40,8 +29,11 @@ $(document).ready(() => {
     customers = new CustomerRepo(data.customerData);
     admin = new Admin(customers, hotel);
     console.log(admin.customers.all[0].name);
-    console.log(delay);
     $('main').text(`here is ${admin.services.all[0].date}`);
   }
   loadFetchData();
-})
+  
+
+
+
+});
