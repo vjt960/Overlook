26da6f00078a4143a79b68d1934b84930f5fe84e
Vinton Teo <taulago@ymail.com>
2019-06-02 00:07:06 -0600

    Link font in html and implement in scss. Create mixin to zero margin and padding.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Hotel from './js/Hotel';
 import CustomerRepo from './js/CustomerRepo';
 import Admin from './js/Admin';
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+import './css/index.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
