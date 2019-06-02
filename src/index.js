1d83bf5963969658e67cff8de84f3ad2410d8f96
Vinton Teo <taulago@ymail.com>
2019-06-02 02:58:57 -0600

    Organize index.js file.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+//--------- IMPORTS --------->
 import $ from 'jquery';
-
 import {xData as data} from '.././fetch-data/data';
 import Rooms from './js/Rooms';
 import Bookings from './js/Bookings';
@@ -7,62 +7,71 @@ import RoomServices from './js/RoomServices';
 import Hotel from './js/Hotel';
 import CustomerRepo from './js/CustomerRepo';
 import Admin from './js/Admin';
+import domUpdates from './js/domUpdates';
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/index.scss';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// Example of how to use an image (need to link to it in index.html)
+import './images/turing-logo.png';
 
+//--------- VARIABLES --------->
+let admin;
+//--------- EVENT LISTENERS --------->
 $(document).ready(() => {
-
-  let rooms;
-  let bookings;
-  let services;
-  let hotel;
-  let customers;
-  let admin;
-
-  async function loadFetchData() {
-    const delay = await data;
-    rooms = new Rooms(data.roomsData);
-    bookings = new Bookings(data.bookingsData, rooms);
-    services = new RoomServices(data.serviceData);
-    hotel = new Hotel(bookings, services);
-    customers = new CustomerRepo(data.customerData);
-    admin = new Admin(customers, hotel);
-    console.log(admin.customers.all[0].name);
-  }
-
-  loadFetchData();
-  
-  $('ul.tabs li').click(function() {
-		let tabID = $(this).attr('data-tab');
-		$('ul.tabs li').removeClass('active');
-		$('.tab-content').removeClass('active');
-		$(this).addClass('active');
-		$(`#${tabID}`).addClass('active');
-  })
-  
-  function showDate() {
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = today.getFullYear();
-    return `${dd}/${mm}/${yyyy}`
-  }
-  
-  function showTime(){
-    let d = new Date();
-    let s = d.getSeconds();
-    let m = d.getMinutes();
-    let h = d.getHours();
-    return `${h}:${m}:${s}`;
-  }
-  
   setInterval(displayTimeNow, 1000);
-  
-  function displayTimeNow() {
-    $('h2').text(`${showDate()} ${showTime()}`);
-  }
-
-
+  loadFetchData();
+  loadMainTab();
 });
+
+$('ul.tabs li').click(function() {
+  let tabID = $(this).attr('data-tab');
+  $('ul.tabs li').removeClass('active');
+  $('.tab-content').removeClass('active');
+  $(this).addClass('active');
+  $(`#${tabID}`).addClass('active');
+});
+
+//--------- FUNCTIONS --------->
+
+async function loadFetchData() {
+  await data;
+  const rooms = new Rooms(data.roomsData);
+  const bookings = new Bookings(data.bookingsData, rooms);
+  const services = new RoomServices(data.serviceData);
+  const hotel = new Hotel(bookings, services);
+  const customers = new CustomerRepo(data.customerData);
+  admin = new Admin(customers, hotel);
+}
+
+function showToday() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+  return `${dd}/${mm}/${yyyy}`
+}
+
+function showTime() {
+  let d = new Date();
+  let s = d.getSeconds();
+  let m = d.getMinutes();
+  let h = d.getHours();
+  h = h < 10 ? `0${h}` : `${h}`;
+  m = m < 10 ? `0${m}` : `${m}`;
+  s = s < 10 ? `0${s}` : `${s}`;
+  return `${h}:${m}:${s}`;
+}
+
+function displayTimeNow() {
+  $('h2').text(`${showToday()} ${showTime()}`);
+}
+
+async function loadMainTab() {
+  await data;
+  // let books = admin.bookings.getCurrentBookings(showToday());
+  // let services = admin.services.getTotalDebt(showToday());
+  // let report = admin.bookings.getOccupancyRatio(showToday());
+  let books = [{date: 'booking1'}, {date: 'booking2'}, {date: 'booking3'}, {date: 'booking4'}];
+  let services = [{date: 'order'}, {date: 'order'}, {date: 'order'}];
+  books.forEach(book => domUpdates.postTodaysBookings(book));
+  services.forEach(order => domUpdates.postTodaysOrders(order));
+}
